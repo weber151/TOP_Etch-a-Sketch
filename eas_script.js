fce48f363a95adfd4549a71a0257c8ec0aef680d
Joe Weber <joe@joeweberdesign.com>
2022-10-10 15:58:41 -0700

    add button to change grid

--- eas_script.js
+++ eas_script.js
@@ -1,15 +1,46 @@
-mainContainer = document.querySelector(".mainContainer");
-
-
 function genGrid(a) {
+    let tempGrid = document.createElement('div');
+    mainGrid.appendChild(tempGrid);
+    tempGrid.classList.add('grid');
     let toAdd = document.createDocumentFragment();
     for(let i = 0; i < a; i++){
-        let newPxl = document.createElement('div');
-        newPxl.classList.add('pxl');
-        newPxl.id = 'r' + i;
-        toAdd.appendChild(newPxl);
+        let newRow = document.createElement('div');
+        newRow.classList.add('row');
+        toAdd.appendChild(newRow);
+        for(let i = 0; i < a; i++) {
+            let newPxl = document.createElement('div');
+            newPxl.classList.add('pxl');
+            newPxl.id = 'r' + i;
+            newRow.appendChild(newPxl);
+        }
     }
-    mainContainer.appendChild(toAdd);
+    tempGrid.appendChild(toAdd);
+    screenEffect();
 }
 
-genGrid(12);
+function removeGrid() {
+    grid = document.querySelector('.grid')
+    grid.remove();
+}
+
+function screenEffect() {
+    screen = document.querySelectorAll(".pxl");
+    for (let x = 0; x < screen.length; x++) {
+        screen[x].addEventListener("mouseenter", function(){
+            screen[x].classList.toggle('pxlOn');
+        })
+    }
+}
+
+function gridInput(){
+    return prompt("Number of pixels per side of grid: ", '25');
+}
+
+mainGrid = document.querySelector("body");
+genGrid(25);
+
+gridBtn = document.querySelector('.gridBtn');
+gridBtn.addEventListener('click', function(){
+    removeGrid();
+    genGrid(gridInput());
+})
